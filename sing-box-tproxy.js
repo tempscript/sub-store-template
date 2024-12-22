3409e35be64d84cbf744eb81d8ede9cfc3e87b48
tempscript <tempscript@outlook.com>
2024-12-22 17:50:07 +0800

    Fix

--- sing-box-tproxy.js
+++ sing-box-tproxy.js
@@ -16,9 +16,10 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['♻️ 自动选择'].includes(i.tag) || ['All'].includes(i.tag)) {
+  if (['♻️ 自动选择'].includes(i.tag) || ['🐸 手动切换'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies))
   }
+ 
   if (['🇭🇰 香港节点'].includes(i.tag) || ['🔯 香港自动'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
   }
