bbe910d7317e568e2a6986718d7a7cca1ca32fc1
wallace <tempscript@outlook.com>
2024-12-27 08:43:10 +0800

    Update sing-box-tun.js

--- sing-box-tun.js
+++ sing-box-tun.js
@@ -1,1 +1,49 @@
+const { type, name } = $arguments
+const compatible_outbound = {
+  tag: 'COMPATIBLE',
+  type: 'direct',
+}
 
+let compatible
+let config = JSON.parse($files[0])
+let proxies = await produceArtifact({
+  name,
+  type: /^1$|col/i.test(type) ? 'collection' : 'subscription',
+  platform: 'sing-box',
+  produceType: 'internal',
+})
+
+config.outbounds.push(...proxies)
+
+config.outbounds.map(i => {
+  if (['All-AUTO'].includes(i.tag) || ['All'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies))
+  }
+ 
+  if (['HK'].includes(i.tag) || ['HK-AUTO'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|kong kong|🇭🇰/i))
+  }
+  if (['JP'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+  }
+  if (['UM'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /美|us|unitedstates|united states|🇺🇸/i))
+  }
+ 
+})
+
+config.outbounds.forEach(outbound => {
+  if (Array.isArray(outbound.outbounds) && outbound.outbounds.length === 0) {
+    if (!compatible) {
+      config.outbounds.push(compatible_outbound)
+      compatible = true
+    }
+    outbound.outbounds.push(compatible_outbound.tag);
+  }
+});
+
+$content = JSON.stringify(config, null, 2)
+
+function getTags (proxies, regex) {
+  return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
